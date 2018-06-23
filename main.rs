2adebaf16d46c415474c813ff06ed8d352f4c285
Jay Mendelsohn <jmendelsohn11@bristolcc.edu>
2018-06-23 15:49:28 -0400

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,41 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+    	eprintln!("Problem parsing arguments: {}", err);
+    	process::exit(1);
+    });
+
+
+    if let Err(e) = minigrep::run(config) {
+    	eprintln!("Application error: {}", e);
+
+    	process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn one_result() {
+		let query = "duct";
+		let contents = "\
+		Rust:
+		safe, fact, productive.
+		Pick three.";
+
+		assert_eq!(
+			vec!["safe, fast, productive."],
+			search(query, contents)
+			);
+	}
+}
